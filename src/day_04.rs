097111506d6eedad8199620f06e6aad7c2c61f9f
tronglt <trongle1711@gmail.com>
2022-12-06 08:32:17 +0700

    refactor: reduce duplication code.

--- src/day_04.rs
+++ src/day_04.rs
@@ -13,30 +13,30 @@ pub fn day_04() {
 }
 
 fn is_fully_contain(pair: &str) -> bool {
-    let pair: Vec<&str> = pair.split(',').collect();
-    let (part_1, part_2): (Vec<usize>, Vec<usize>) = (
-        pair[0].split('-').map(|n| n.parse().unwrap()).collect(),
-        pair[1].split('-').map(|n| n.parse().unwrap()).collect(),
-    );
+    let (part_1, part_2) = parse_pair(pair);
 
-    part_1[0] <= part_2[0] && part_1[1] >= part_2[1]
-        || part_2[0] <= part_1[0] && part_2[1] >= part_1[1]
+    part_1[0] <= part_2[0] && part_1.last() >= part_2.last()
+        || part_2[0] <= part_1[0] && part_2.last() >= part_1.last()
 }
 
 fn is_overlap(pair: &str) -> bool {
-    let pair: Vec<&str> = pair.split(',').collect();
-    let (part_1, part_2): (Vec<usize>, Vec<usize>) = (
-        pair[0].split('-').map(|n| n.parse().unwrap()).collect(),
-        pair[1].split('-').map(|n| n.parse().unwrap()).collect(),
-    );
+    let (part_1, part_2) = parse_pair(pair);
 
-    let range_1 = (part_1[0]..=part_1[1]).collect::<Vec<usize>>();
-    let range_2 = (part_2[0]..=part_2[1]).collect::<Vec<usize>>();
+    part_1.contains(&part_2[0])
+        || part_1.contains(&part_2.last().unwrap())
+        || part_2.contains(&part_1[0])
+        || part_2.contains(&part_1.last().unwrap())
+}
 
-    range_1.contains(&range_2[0])
-        || range_1.contains(&range_2.last().unwrap())
-        || range_2.contains(&range_1[0])
-        || range_2.contains(&range_1.last().unwrap())
+fn parse_pair(text: &str) -> (Vec<usize>, Vec<usize>) {
+    let pair: Vec<&str> = text.split(',').collect();
+    let part_1: Vec<usize> = pair[0].split('-').map(|n| n.parse().unwrap()).collect();
+    let part_2: Vec<usize> = pair[1].split('-').map(|n| n.parse().unwrap()).collect();
+
+    (
+        (part_1[0]..=part_1[1]).collect::<Vec<usize>>(),
+        (part_2[0]..=part_2[1]).collect::<Vec<usize>>(),
+    )
 }
 
 #[cfg(test)]
@@ -57,5 +57,13 @@ mod tests {
         assert!(is_overlap("2-6,4-8"));
         assert!(!is_overlap("2-4,6-8"));
         assert!(!is_overlap("2-3,4-5"));
+    }
+
+    #[test]
+    fn test_parse_pair() {
+        let (part_1, part_2) = parse_pair("5-7,7-9");
+
+        assert_eq!(vec![5, 6, 7], part_1);
+        assert_eq!(vec![7, 8, 9], part_2);
     }
 }
